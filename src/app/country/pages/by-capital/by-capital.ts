a18cd9a8aaf1c47a689e869d7e617e8e2497a689
luisda <luisdacv18@gmail.com>
2025-10-07 12:09:47 -0400

    first commit

---
+++ src/app/country/pages/by-capital/by-capital.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital',
+  imports: [],
+  templateUrl: './by-capital.html',
+  styleUrl: './by-capital.css'
+})
+export class ByCapital {
+
+}
---
+++ src/app/country/pages/see-country/see-country.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-see-country',
+  imports: [],
+  templateUrl: './see-country.html',
+  styleUrl: './see-country.css'
+})
+export class SeeCountry {
+
+}
